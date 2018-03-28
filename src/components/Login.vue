67e54679228591495061ea78991cbce82632c31c
cj <jclazz@outlook.com>
2018-03-28 18:25:13 +0800

    add page

---
+++ src/components/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h3>{{ title }}</h3>
+    <input v-bind:type="accType" v-bind:value="acc" title="login name"/>
+    <input type="password"/>
+    <button>{{ loginBtnTitle }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  props: {
+    title: {
+      default: 'login'
+    },
+    accType: {
+      default: 'text'
+    },
+    acc: {
+      default: ''
+    },
+    loginBtnTitle: {
+      default: 'ok'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
