cd932832c5dd45b0faf3b6657fb52450e6c56dd0
cj <jclazz@outlook.com>
2018-03-30 08:56:10 +0800

    move file

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/pages/HelloWorld'
 // import Login from '@/components/Login'
-import Home from '@/components/Home'
-import UserPermissionAssignment from '@/components/UserPermissionAssignment'
+import Home from '@/pages/Home'
+import UserPermissionAssignment from '@/pages/UserPermissionAssignment'
 
 Vue.use(Router)
 
@@ -18,8 +18,8 @@ export default new Router({
       name: 'Home',
       component: Home
     }, {
-      path: '/user-permission-assignment',
-      name: 'Home',
+      path: '/user-permission',
+      name: 'UserPermission',
       component: UserPermissionAssignment
     }
   ]
